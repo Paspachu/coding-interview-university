0b553039a92fbe0e620b9d438d780b5259d59957
Minho Choi <minhochoi.official@gmail.com>
2024-06-13 01:30:24 +0000

    Practiced Linked List

--- README.md
+++ README.md
@@ -615,14 +615,14 @@ if you can identify the runtime complexity of different algorithms. It's a super
 
 - ### Linked Lists
     - [ ] Description:
-    	- [ ] [Linked Lists CS50 Harvard University](https://www.youtube.com/watch?v=2T-A_GFuoTo&t=650s) - this builds the intuition.
+    	- [X] [Linked Lists CS50 Harvard University](https://www.youtube.com/watch?v=2T-A_GFuoTo&t=650s) - this builds the intuition.
         - [ ] [Singly Linked Lists (video)](https://www.coursera.org/lecture/data-structures/singly-linked-lists-kHhgK)
         - [ ] [CS 61B - Linked Lists 1 (video)](https://archive.org/details/ucberkeley_webcast_htzJdKoEmO0)
         - [ ] [CS 61B - Linked Lists 2 (video)](https://archive.org/details/ucberkeley_webcast_-c4I3gFYe3w)
         - [ ] [[Review] Linked lists in 4 minutes (video)](https://youtu.be/F8AbOfQwl1c)
     - [ ] [C Code (video)](https://www.youtube.com/watch?v=QN6FPiD0Gzo)
             - not the whole video, just portions about Node struct and memory allocation
-    - [ ] Linked List vs Arrays:
+    - [X] Linked List vs Arrays:
         - [Core Linked Lists Vs Arrays (video)](https://www.coursera.org/lecture/data-structures-optimizing-performance/core-linked-lists-vs-arrays-rjBs9)
         - [In The Real World Linked Lists Vs Arrays (video)](https://www.coursera.org/lecture/data-structures-optimizing-performance/in-the-real-world-lists-vs-arrays-QUaUd)
     - [ ] [Why you should avoid linked lists (video)](https://www.youtube.com/watch?v=YQs6IC-vgmo)
@@ -630,7 +630,23 @@ if you can identify the runtime complexity of different algorithms. It's a super
         (for when you pass a pointer to a function that may change the address where that pointer points)
         This page is just to get a grasp on ptr to ptr. I don't recommend this list traversal style. Readability and maintainability suffer due to cleverness.
         - [Pointers to Pointers](https://www.eskimo.com/~scs/cclass/int/sx8.html)
-    - [ ] Implement (I did with tail pointer & without):
+    - [X] Implement (I did with tail pointer & without):
+        - [X] size() - returns the number of data elements in the list
+        - [X] empty() - bool returns true if empty
+        - [X] value_at(index) - returns the value of the nth item (starting at 0 for first)
+        - [X] push_front(value) - adds an item to the front of the list
+        - [X] pop_front() - remove the front item and return its value
+        - [X] push_back(value) - adds an item at the end
+        - [X] pop_back() - removes end item and returns its value
+        - [X] front() - get the value of the front item
+        - [X] back() - get the value of the end item
+        - [X] insert(index, value) - insert value at index, so the current item at that index is pointed to by the new item at the index
+        - [X] erase(index) - removes node at given index
+        - [X] value_n_from_end(n) - returns the value of the node at the nth position from the end of the list
+        - [X] reverse() - reverses the list
+        - [X] remove_value(value) - removes the first item in the list with this value
+    - [ ] Doubly-linked List
+        - [ ] [Description (video)](https://www.coursera.org/lecture/data-structures/doubly-linked-lists-jpGKD)
         - [ ] size() - returns the number of data elements in the list
         - [ ] empty() - bool returns true if empty
         - [ ] value_at(index) - returns the value of the nth item (starting at 0 for first)
@@ -645,9 +661,6 @@ if you can identify the runtime complexity of different algorithms. It's a super
         - [ ] value_n_from_end(n) - returns the value of the node at the nth position from the end of the list
         - [ ] reverse() - reverses the list
         - [ ] remove_value(value) - removes the first item in the list with this value
-    - [ ] Doubly-linked List
-        - [Description (video)](https://www.coursera.org/lecture/data-structures/doubly-linked-lists-jpGKD)
-        - No need to implement
 
 - ### Stack
     - [ ] [Stacks (video)](https://www.coursera.org/lecture/data-structures/stacks-UdKzQ)
---
+++ codes/Linked List/linkedlist.py
@@ -0,0 +1,281 @@
+# Structure of a node
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+# Structure of a linked list
+class LinkedList:
+    def __init__(self, head = None):
+        self.head = head
+
+    def print_list(self, listname):
+        # Starting point
+        current = self.head
+        output = "The list " + listname + " looks like: "
+        # Loop through the list until the end
+        while current:
+            output += str(current.value) + ", "
+            current = current.next
+        # Print the list excluding the last two elements in the string, which are ", "
+        print(output[:-2])
+
+    # Get the size of the list
+    def size(self):
+        # Starting point
+        current = self.head
+        s = 0
+        # Loop through list until the end
+        while current:
+            current = current.next
+            s += 1
+        return s
+
+    # Check if the list is empty
+    def empty(self):
+        if not self.head:
+            return True        
+        return False
+    
+    # Accessing the value at an index
+    def value_at(self, index):
+        # TODO: implement negative index
+        # Check if index is out of bound
+        if self.size() < index or index < 0:
+            raise Exception("Index out of bound!")
+        # Starting point
+        current = self.head
+        i = 0
+        while i < index:
+            current = current.next
+            i += 1
+        return current.value
+    
+    def push_front(self, value):
+        # Create a new node to add
+        new_head = Node(value)
+        # If the list is empty then add the new value to list and finish 
+        if self.empty():
+            self.head = new_head
+        else:
+            # New head points to the original head
+            new_head.next = self.head
+            # Update the head of the list to new head
+            self.head = new_head
+    
+    def pop_front(self):
+        # If the list is empty then raise Exception 
+        if self.empty():
+            raise Exception("The list is empty!")
+        # Save the value to return
+        output = self.head.value
+        # Update the head of list
+        self.head = self.head.next
+        # Return the first item in the list
+        return output
+
+    def push_back(self, value):
+        # Create a new node to add
+        new_tail = Node(value)
+        # If the list is empty then add the new value to list and finish 
+        if self.empty():
+            self.head = new_tail
+        else:
+            # Get current tail of the list
+            current = self.head
+            while current.next:
+                current = current.next
+            # Add the new tail
+            current.next = new_tail
+    
+    def pop_back(self):
+        # If the list is empty then raise Exception 
+        if self.empty():
+            raise Exception("The list is empty!")
+        # Get item one before the tail of the list
+        current = self.head
+        while current.next.next:
+            current = current.next
+        # Save the value to return
+        output = current.next.value
+        # Update the tail of the list
+        current.next = None
+        return output
+
+    def front(self):
+        # Get the first item in the list
+        return self.head.value
+    
+    def back(self):
+        # Go to the tail of the list
+        current = self.head
+        while current.next:
+            current = current.next
+        # Return the last item in the list
+        return current.value
+
+    def insert(self, index, value):
+        # Check if index is valid
+        if self.size() < index or index < 0:
+            raise Exception("Index is out of bound!")
+        # Create a new node to add
+        new_node = Node(value)
+        # Go to one before the index in the list
+        current = self.head
+        i = 0
+        while i < index - 1:
+            current = current.next
+            i += 1
+        # New node points to the item that is currently at the index
+        new_node.next = current.next
+        # Connect the item one before the index to new node
+        current.next = new_node
+
+    def erase(self, index):
+        # Check if index is valid
+        if self.size() < index or index < 0:
+            raise Exception("Index is out of bound!")
+        # Go to one before the index in the list
+        current = self.head
+        i = 0
+        while i < index - 1:
+            current = current.next
+            i += 1
+        # Connect the item one before the index to the item one after the index
+        current.next = current.next.next
+    
+    def value_n_from_end(self, n):
+        # Get the target index
+        index = self.size() - n
+        # Check if index is valid
+        if index < 0:
+            raise Exception("The input value is not valid.")
+        # Go to the index in the list
+        current = self.head
+        i = 0
+        while i < index:
+            current = current.next
+            i += 1
+        # Return the value at the index
+        return current.value
+    
+    def reverse(self):
+        # Create a new head
+        new_head = None
+        # Loop through the list
+        while self.head:
+            # Move the head and change the direction of the head. The new head becomes the previous item in the list
+            self.head.next, self.head, new_head = new_head, self.head.next, self.head
+        # Define the new head
+        self.head = new_head
+
+    def remove_value(self, value):
+        # If the first item has the equal value, then simply change the head of list to the second item in the list
+        if self.head.value == value:
+            self.head = self.head.next
+        else:
+            # Starting point (we know that head is not equal to the input value due to if statement)
+            current = self.head
+            # Loop through the items in the list
+            while current.next:
+                # If the equal value is found simply connect the one before to one after
+                if current.next.value == value:
+                    current.next = current.next.next
+                    break
+                current = current.next
+            # TODO: flag when the value is not found in the list
+
+
+# For test, initialize nodes with values corresponding the digits of pi
+n1 = Node(3)
+n2 = Node(1)
+n3 = Node(4)
+n4 = Node(1)
+n5 = Node(5)
+
+# Link the nodes
+n1.next = n2
+n2.next = n3
+n3.next = n4
+n4.next = n5
+
+# Test for getting the size of the list
+l1 = LinkedList(n1)
+print("Size of l1: {}".format(l1.size()))
+
+# Test for checking empty list
+l2 = LinkedList()
+print("Is l2 empty? {}".format(l2.empty()))
+# If head node is initialized, the list is no longer empty
+l2.head = n4
+print("Is l2 empty? {}".format(l2.empty()))
+print("Size of l2: {}".format(l2.size()))
+
+# Test for accessing value at an index
+print("The value at index 2 of l1 is {}".format(l1.value_at(2)))
+l1.print_list("l1")
+# print(l2.value_at(3))
+# print(l1.value_at(-1))
+
+# Test for push front
+l1.push_front(1)
+l1.print_list("l1")
+l3 = LinkedList()
+l3.push_front(3)
+l3.print_list("l3")
+
+# Test for pop front
+print("The first item in list l1 is: {}".format(l1.pop_front()))
+l1.print_list("l1")
+# l4 = LinkedList()
+# print(l4.pop_front())
+
+# Test for push back
+l1.push_back(9)
+l1.print_list("l1")
+l5 = LinkedList()
+l5.push_back(3)
+l5.print_list("l5")
+
+# Test for pop back
+print("The last item in list l1 is: {}".format(l1.pop_back()))
+l1.print_list("l1")
+# l6 = LinkedList()
+# print(l6.pop_back())
+
+# Test for front
+print("The first item in the list l1 is {}".format(l1.front()))
+
+# Test for front
+print("The last item in the list l1 is {}".format(l1.back()))
+
+# Test for insert
+l1.print_list("l1")
+l1.insert(2, 9)
+l1.print_list("l1")
+l1.insert(4, 7)
+l1.print_list("l1")
+
+# Test for erase
+l1.erase(2)
+l1.print_list("l1")
+l1.erase(3)
+l1.print_list("l1")
+
+# Test for value n from end
+print("The 1st value from the end of list l1 is {}".format(l1.value_n_from_end(1)))
+print("The 3rd value from the end of list l1 is {}".format(l1.value_n_from_end(3)))
+# print("The 6th value from the end of list l1 is {}".format(l1.value_n_from_end(6)))
+
+# Test for reverse
+l1.print_list("l1")
+l1.reverse()
+l1.print_list("l1")
+
+# Test for remove value
+l1.remove_value(1)
+l1.print_list("l1")
+l1.remove_value(6)
+l1.print_list("l1")
+l1.remove_value(1)
+l1.print_list("l1")
